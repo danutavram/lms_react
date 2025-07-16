4d7556cc982df2f309603c0959a8bb7566ae62df
danutavram <avramnicolaedan@gmail.com>
2025-07-16 12:28:16 +0300

    testimonials

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -10,6 +10,7 @@ export const AppContextProvider = (props)=>{
     const navigate = useNavigate()
 
     const [allCourses, setAllCourses] = useState([])
+    const [isEducator, setIsEducator] = useState(true)
 
     // Fetch All Courses
     const fetchAllCourses = async()=>{
@@ -33,7 +34,8 @@ export const AppContextProvider = (props)=>{
     },[])
 
     const value = {
-        currency, allCourses, navigate, calculateRating
+        currency, allCourses, navigate, calculateRating,
+        isEducator, setIsEducator
 
     }
     return (
