165e7a9b73efd4dd0b69bb75cc9a9366be5a78d2
danutavram <avramnicolaedan@gmail.com>
2025-07-21 17:42:43 +0300

    changes

--- server/server.js
+++ server/server.js
@@ -1,27 +1,23 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv/config';
+import bodyParser from 'body-parser';
 import connectDB from './configs/mongodb.js';
 import { clerkWebhooks } from './controllers/webhooks.js';
 
-// Initialize Express
 const app = express();
 
-// Connect to database
 await connectDB();
 
-// Middleware
 app.use(cors());
 
-// Routes
-app.get('/', (req, res) => res.send('API Working'))
-app.post('/clerk', express.json(), clerkWebhooks)
+// Test route
+app.get('/', (req, res) => res.send('API Working'));
 
-// Port
+// Clerk webhook route
+app.post('/clerk', bodyParser.raw({ type: 'application/json' }), clerkWebhooks);
+
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 export default app;
