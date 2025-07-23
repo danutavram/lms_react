3b7da789f2c9643452c923b2d7f0f9112df01368
danutavram <avramnicolaedan@gmail.com>
2025-07-23 18:38:17 +0300

    Purchase.js

--- server/routes/educatorRoutes.js
+++ server/routes/educatorRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {addCourse, updateRoleToEducator, getEducatorCourses} from '../controllers/educatorController.js';
+import {addCourse, updateRoleToEducator, getEducatorCourses, educatorDashboardData, getEnrolledStudentsData} from '../controllers/educatorController.js';
 import upload from '../configs/multer.js';
 import {protectEducator} from '../middlewares/authMiddleware.js';
 
@@ -9,5 +9,6 @@ const educatorRouter = express.Router()
 educatorRouter.get('/update-role', updateRoleToEducator)
 educatorRouter.post('/add-course', upload.single('image'), protectEducator, addCourse)
 educatorRouter.get('/courses', protectEducator, getEducatorCourses)
+educatorRouter.get('/enrolled-students', protectEducator, educatorDashboardData);
 
 export default educatorRouter;
