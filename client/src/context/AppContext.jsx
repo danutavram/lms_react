5cce8e2fc9fb8586273cd85c2692bd7936ea2ee4
danutavram <avramnicolaedan@gmail.com>
2025-07-27 18:28:58 +0300

    bug fixed

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
-import { useNavigate } from "react-router-dom";
+import { data ,useNavigate } from "react-router-dom";
 import humanizeDuration from "humanize-duration";
 import { useAuth, useUser  } from "@clerk/clerk-react";
 import axios from "axios";
